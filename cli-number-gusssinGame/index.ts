ee689c1a6b98a4a86ee5912a9a53d2160aff4e88
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-03-24 22:26:49 +1200

    This is a cli number gussing Game

---
+++ cli-number-gusssinGame/index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/node
+import inquirer from 'inquirer';
+
+
+
+// to generate random number
+const randomNumber = Math.floor(Math.random()*6+1)
+
+//To take the input from user
+const answers = await inquirer.prompt(
+    [
+        {
+        message:"Please Gusse a Number Between 1 to 6: ",
+        name:"UserGussedNumber",
+        type:'number'
+        }
+    ]
+)
+// To comparing user input with computer random number
+if(answers.UserGussedNumber === randomNumber ){
+console.log(`Congratulation! Your number gusse is correct.\n
+          Your gusse number is ${answers.UserGussedNumber}
+          Computer gusse number is ${randomNumber}`)
+}
+else if(answers.UserGussedNumber >= 7){
+  console.log('You can not enter number grater than 6')
+}
+else{
+  console.log(`Sorry,Your gusse number is not correct\n
+  Your gusse number is ${answers.UserGussedNumber}
+  Computer gusse number is ${randomNumber}`);
+  
+}
